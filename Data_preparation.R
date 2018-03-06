c740b852e95c731fcd5e2040567b7cdb0c4d0e6b
Yulia Dukach <36727249+DukachY@users.noreply.github.com>
2018-03-06 22:13:39 +0200

    Update Data_preparation.R

--- Data_preparation.R
+++ Data_preparation.R
@@ -4,14 +4,11 @@ library(sna)
 library(network)
 library(devtools)
 
-setwd("~/Documents/OneDrive/RRR/ДИСЕР")
-
 #alldata = fuul database for 2011-2013 years for Ukraine
-alldata <- read.csv("Edges all years.csv", header=T, as.is=T, sep = ";")
-
+alldata <- read.csv("Edges all years.csv", header = T, as.is = T, sep = ";")
 
 # allnet = full network
-allnet <- network(alldata[,5:6], matrix.type="edgelist", directed = F)
+allnet <- network(alldata[,5:6], matrix.type = "edgelist", directed = F)
 summary(allnet, print.adj = F)
 network.size(allnet) # vertices = 1086
 plot(allnet) # preliminary visualization
@@ -31,16 +28,16 @@ set.vertex.attribute(allnet, "pr-ers13", allvnames$Protesters.2013)
 set.vertex.attribute(allnet, "sum.pr", allvnames$Summ.Protests)
 set.vertex.attribute(allnet, "sum.pr-ers", allvnames$Summ.Protesters)
 set.vertex.attribute(allnet, "cat.sum.pr-ers", allvnames$Categories.by.summ.of.protesters)
-set.vertex.attribute(allnet, "degree.centrality", degree(allnet,gmode="graph"))
-set.vertex.attribute(allnet, "closeness.centrality", closeness(allnet,gmode="graph"))
-set.vertex.attribute(allnet, "betweenness.centrality", betweenness(allnet,gmode="graph"))
+set.vertex.attribute(allnet, "degree.centrality", degree(allnet,gmode = "graph"))
+set.vertex.attribute(allnet, "closeness.centrality", closeness(allnet,gmode = "graph"))
+set.vertex.attribute(allnet, "betweenness.centrality", betweenness(allnet,gmode = "graph"))
 
-
-delete.vertices(allnet, isolates(allnet)) # delite all isolated vertices
+# delite all isolated vertices
+delete.vertices(allnet, isolates(allnet)) 
 
 # Setting the edge attributes - date, oblast and year
 date <- as.Date(alldata$Event.start.date, format = "%d.%m.%Y")
-set.edge.attribute(allnet, "date", alldata$Event.start.date) # точная дата начала протеста
-set.edge.attribute(allnet, "oblast", alldata$Oblast.number) # номер области
-set.edge.attribute(allnet, "year", alldata$Year) # год
+set.edge.attribute(allnet, "date", alldata$Event.start.date) # data when protest started
+set.edge.attribute(allnet, "oblast", alldata$Oblast.number) # number of the oblast
+set.edge.attribute(allnet, "year", alldata$Year) # year
 list.edge.attributes(allnet)
